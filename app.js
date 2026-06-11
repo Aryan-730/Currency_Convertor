84c81d226d427ddb1d3b8c215c9dade71b6e2aa3
Aryan-730 <kedararyan77@gmail.com>
2026-06-12 01:12:50 +0530

    modified option name and add country code as text

--- app.js
+++ app.js
@@ -3,28 +3,34 @@ let Base_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/cu
 const dropdowns = document.querySelectorAll(".dropdown select");
 
 for (let select of dropdowns) {
-    for (currencycode in countries) {
+    for (let currencyCode in countries) {
         let newOption = document.createElement("option");
-        newOption.innerText = currencycode;
-        newOption.value = currencycode;
-        if (select.name === "from" && currencycode === "INR") {
+        newOption.innerText = countryNames[currencyCode];  
+        newOption.value = currencyCode;                    
+
+        if (select.name === "from" && currencyCode === "INR") {
             newOption.selected = "selected";
-        } else if (select.name === "to" && currencycode === "USD") {
+        } else if (select.name === "to" && currencyCode === "USD") {
             newOption.selected = "selected";
         }
+
         select.append(newOption);
     }
 
     select.addEventListener("change", (evt) => {
         updateFlag(evt.target);
-    })
+    });
 }
 
 const updateFlag = (element) => {
-    let currencycode = element.value;
-    let countryCode = countries[currencycode];
+    let currencyCode = element.value;
+    let countryCode  = countries[currencyCode];
     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-    
-    let img = element.parentElement.querySelector("img");
+
+    let selector  = element.closest(".option-selector");
+    let img       = selector.querySelector("img");
+    let nameLabel = selector.querySelector(".countryName");
+
     img.src = newSrc;
+    nameLabel.textContent = currencyCode;  
 }
