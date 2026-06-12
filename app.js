44cb9686381df53fe403ab72bd05ac1d123ae365
Aryan-730 <kedararyan77@gmail.com>
2026-06-13 00:00:15 +0530

    added features with logic

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-let Base_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+let Base_URL = 'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies';
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const formBtn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+const resultDiv = document.querySelector(".resultDiv");
+const amountInput = document.querySelector(".amount-input");
+
 
 for (let select of dropdowns) {
     for (let currencyCode in countries) {
@@ -34,3 +41,32 @@ const updateFlag = (element) => {
     img.src = newSrc;
     nameLabel.textContent = currencyCode;  
 }
+
+
+
+formBtn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount-input");
+    let amountVal = amount.value;
+    if (amountVal === "" || amountVal < 1) {
+        amountVal = 1;
+        amount.value = "1";
+    }
+
+    const URL = `${Base_URL}/${fromCurr.value.toLowerCase()}.json`; 
+    let response = await fetch(URL);
+    let data = await response.json();
+
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    let finalAmount = (amountVal * rate).toFixed(2);
+
+    console.log(finalAmount);
+
+    msg.innerText = `${amountVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+
+    resultDiv.classList.remove("hide");
+}); 
+
+amountInput.addEventListener("input", () => {
+    resultDiv.classList.add("hide");
+});
